feb2f07a25a2c35c68c19d450befc6b37c3903f7
megatron10 <megatron10599@gmail.com>
2025-08-03 12:27:39 +0530

    big things start small

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastcore.all import *
+from fastai.vision.all import *
+import gradio as gr
+
+learn = load_learner('test.pkl')
+categories = ('anime', 'phineas and ferb', 'tom & jerry')
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float, probs)))
+
+demo = gr.Interface(
+    fn=classify_image,
+    inputs=["image"],
+    outputs=["label"],
+    examples=["perry.jpg","aot.jpg","t&j.jpg"]
+)
+
+demo.launch()
